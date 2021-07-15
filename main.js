e4e4dc00d5eb1deb7e31297ab3d0b37b9385f72e
calixd7 <calixd7@gmail.com>
2021-07-15 14:32:54 -0400

    scroll feature

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
